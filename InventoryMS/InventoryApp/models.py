e704b0712910e5369e37ee4ee5151bc0e9be94f0
Maz <mazoolander@me.com>
2025-01-27 20:17:41 +0100

    Added Product model and views, installed Pillow, and created initial migrations

--- InventoryMS/InventoryApp/models.py
+++ InventoryMS/InventoryApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+# Product table
+class Product(models.Model):
+    product_id = models.AutoField(primary_key=True)
+    product_name = models.CharField(max_length=100)
+    product_category = models.CharField(max_length=100)
+    product_sku = models.CharField(max_length=50, unique=True)
+    product_price = models.FloatField()
+    product_quantity = models.IntegerField()
+    product_weight = models.FloatField()
+    product_color = models.CharField(max_length=100)
+    product_size = models.CharField(max_length=50)
+    product_supplier = models.CharField(max_length=300)
+    product_description = models.TextField()
+    product_image = models.ImageField(upload_to='product_images/')
+
+    def __str__(self):
+        return self.product_name
